14b215aa52bdd6a9388811483a5accc4ed4afe81
aa.malcev2 <aa.malcev2@tensor.ru>
2020-07-02 01:29:36 +0300

    Download theme sources automatically.

---
+++ cli/prepare.js
@@ -0,0 +1,36 @@
+const {parse, join, resolve} = require('path');
+const {copy, download, mkdir, remove, unzip} = require('../lib/file');
+
+const manualDependencies = require('../package.json').manualDependencies || {};
+const TEMP_DIR = '.temp';
+
+async function processDependency(url, tempFile, innerPath, targetPath) {
+	console.log(`Downloading "${url}" to "${tempFile}"`);
+
+	//await remove(tempFile);
+	//await download(url, tempFile);
+
+	const tempParts = parse(tempFile);
+	const tempPath = tempParts.dir;
+	console.log(`Unzipping "${tempFile}" to "${tempPath}"`);
+	await unzip(tempFile, resolve(tempPath));
+
+	const sourcePath = join(tempPath, innerPath);
+	console.log(`Copying "${sourcePath}" to "${targetPath}"`);
+	await mkdir(targetPath);
+	await copy(sourcePath, targetPath);
+}
+
+console.log('Processing manual dependencies');
+
+for (const [targetPath, {url, path}] of Object.entries(manualDependencies)) {
+	const parsedUrl = new URL(url);
+	const parsedPathName = parse(parsedUrl.pathname);
+	const tempFile = join(TEMP_DIR, parsedPathName.base);
+	processDependency(url, tempFile, path, targetPath).then(() => {
+		console.log(`URL "${url}" processed sucessfully`);
+	}).catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
+}
