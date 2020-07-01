dfb1d397e4490388c522611e59640c29c450dedb
aa.malcev2 <aa.malcev2@tensor.ru>
2020-07-02 01:41:25 +0300

    Re-build all

--- cli/prepare.js
+++ cli/prepare.js
@@ -1,5 +1,5 @@
 const {parse, join, resolve} = require('path');
-const {copy, download, mkdir, remove, unzip} = require('../lib/file');
+const {copy, download, mkdir, remove, rmdir, unzip} = require('../lib/file');
 
 const manualDependencies = require('../package.json').manualDependencies || {};
 const TEMP_DIR = '.temp';
@@ -7,18 +7,23 @@ const TEMP_DIR = '.temp';
 async function processDependency(url, tempFile, innerPath, targetPath) {
 	console.log(`Downloading "${url}" to "${tempFile}"`);
 
-	//await remove(tempFile);
-	//await download(url, tempFile);
+	await remove(tempFile);
+	await download(url, tempFile);
 
 	const tempParts = parse(tempFile);
 	const tempPath = tempParts.dir;
 	console.log(`Unzipping "${tempFile}" to "${tempPath}"`);
 	await unzip(tempFile, resolve(tempPath));
 
+	console.log(`Removing "${targetPath}"`);
+	await rmdir(targetPath);
+
 	const sourcePath = join(tempPath, innerPath);
 	console.log(`Copying "${sourcePath}" to "${targetPath}"`);
 	await mkdir(targetPath);
 	await copy(sourcePath, targetPath);
+
+	console.log(`URL "${url}" processed sucessfully`);
 }
 
 console.log('Processing manual dependencies');
@@ -27,9 +32,7 @@ for (const [targetPath, {url, path}] of Object.entries(manualDependencies)) {
 	const parsedUrl = new URL(url);
 	const parsedPathName = parse(parsedUrl.pathname);
 	const tempFile = join(TEMP_DIR, parsedPathName.base);
-	processDependency(url, tempFile, path, targetPath).then(() => {
-		console.log(`URL "${url}" processed sucessfully`);
-	}).catch((err) => {
+	processDependency(url, tempFile, path, targetPath).catch((err) => {
 		console.error(err);
 		process.exit(1);
 	});
